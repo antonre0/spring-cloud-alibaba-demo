37d60e4dd70b80277de102933bbca7fc8bfba0b6
chenandong <chenandong>
2020-08-14 10:14:30 +0800

    首次提交

---
+++ nacos-provider/src/main/java/com/ncars/nacos/pojo/dto/TestDto.java
@@ -0,0 +1,13 @@
+package com.ncars.nacos.pojo.dto;
+
+public class TestDto {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ nacos-provider/src/main/java/com/ncars/nacos/pojo/vo/TestVo.java
@@ -0,0 +1,13 @@
+package com.ncars.nacos.pojo.vo;
+
+public class TestVo {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
